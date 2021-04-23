6d7cc1303b77f1586a5c33521181d281ffbb916b
jonandergomez <jon@dsic.upv.es>
2021-04-23 17:24:51 +0200

    Update of scripts

--- scripts/evaluate_classifier.sh
+++ scripts/evaluate_classifier.sh
@@ -3,6 +3,12 @@
 #model_format='onnx'
 model_format='eddl'
 
-#python python/evaluate_classifier.py  --index etc/index_training.txt --model 1a --model-filename models/model_classifier_1a-9.${model_format} 
-#python python/evaluate_classifier.py  --index etc/index_test.txt     --model 1a --model-filename models/model_classifier_1a-9.${model_format} 
- python python/evaluate_classifier.py  --index etc/index_mini.txt     --model 1a --model-filename models/model_classifier_1a-18.${model_format} 
+model_id='1a'
+epoch='199'
+model_filename="models/model_classifier_${model_id}-${epoch}.${model_format}"
+
+#index_dataset="etc/index_training.txt"
+index_dataset="etc/index_test.txt"
+#index_dataset="etc/index_mini.txt"
+
+python python/evaluate_classifier.py  --index ${index_dataset}  --model ${model_id}  --model-filename ${model_filename}
