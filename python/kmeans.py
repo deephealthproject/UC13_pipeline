00d8e5931565dad871f5d00730b657699c49029e
Jon Ander Gomez Adrian <jon@eibds01.mbda>
2021-05-31 17:37:02 +0200

    Update of indexes and summaries

---
+++ python/kmeans.py
@@ -0,0 +1,89 @@
+import os
+import sys
+import numpy
+
+
+from data_utils_eeg import SimpleDataGenerator
+
+from machine_learning import KMeans, kmeans_load
+
+
+if __name__ == '__main__':
+
+    models_filename = 'models/kmeans.14'
+    task = 'evaluate'
+    index_filenames = list()
+    for i in range(1, len(sys.argv)):
+        param = sys.argv[i]
+        if sys.argv[i] == '--index':
+            index_filenames.append(sys.argv[i+1])
+        elif sys.argv[i] == '--task':
+            task = sys.argv[i+1]
+
+    if task not in ['training', 'evaluate', 'compute-empirical-distribution']:
+        raise Exception(f'Unrecognized task {task}')
+
+
+    if len(index_filenames) == 0:
+        raise Exception('Nothing can be done without data, my friend!')
+
+
+    dg = SimpleDataGenerator(index_filenames, verbose = 1)
+
+    if task == 'training':
+
+        if os.path.exists(models_filename):
+            print()
+            print(f'The file {models_filename} already exists, please, remove it before invoking this program again to do the computation!')
+            print()
+            sys.exit(0)
+
+        kmeans = KMeans(n_clusters = 50, modality = 'original-k-Means')
+
+        # generate a list of samples that contains 'kmeans.n_clusters' samples a least
+        X = list()
+        while len(X) < kmeans.n_clusters:
+            x, l = dg.next()
+            if x is None or len(x) == 0: break
+            X += [_ for _ in x]
+
+        # do initialization
+        kmeans.original_k_means_init(dg.next_block(X))
+
+        # sequentially process all the remaining samples
+        chars = '|/-\\'
+        i = 0
+        while True:
+            #X, L = dg.next_block()
+            x, l = dg.next()
+            if x is None or len(x) == 0: break
+            kmeans.original_k_means_iteration(x)
+            print(chars[i],  end = '\r', flush = True)
+            i = (i + 1) % 4
+        
+        kmeans.save('models/kmeans.14')
+
+    elif task == 'compute-empirical-distribution':
+        kmeans = kmeans_load('models/kmeans.14')
+
+    elif task == 'evaluate':
+        kmeans = kmeans_load('models/kmeans.14')
+        i = 0
+        for x in kmeans.cluster_centers_:
+            if sum(numpy.isnan(x)) > 0:
+                print('NaN')
+            #print("%4d" % i, " ".join("{:10.6f}".format(_) for _ in x))
+            i += 1
+
+        counter = 0
+        nan_counters = [0] * kmeans.cluster_centers_.shape[1]
+        while True:
+            x, l = dg.next()
+            if x is None: break
+            #if sum(numpy.isnan(x.any())) > 0:
+            for i in range(len(nan_counters)):
+                nan_counters[i] +=  sum(numpy.isnan(x[:, i]))
+            counter += 1
+            print("\r %s  %20d" % (" ".join("{:8d}".format(_) for _ in nan_counters), counter), end = ' ')
+        print()
+        print(f'NaN counter {sum(nan_counters)}')
---
+++ scripts/generate_indices.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+find ../clean_signals/ -type f -name "*.fbank.pbz2" | egrep -v "chb11|chb17|chb18|chb20" | sed 's/\.fbank.pbz2//g' | sort >etc/index_all.txt
+
+
+for n in {1..24}
+do
+    echo ${n}
+    pattern=$(printf "chb%02d" ${n})
+    grep ${pattern} etc/index_all.txt >etc/index_${pattern}.txt
+
+    l=$(wc -l etc/index_${pattern}.txt | cut -f1 -d' ')
+    if [ ${l} -lt 1 ]
+    then
+        rm -f etc/index_${pattern}.txt
+    fi
+done
