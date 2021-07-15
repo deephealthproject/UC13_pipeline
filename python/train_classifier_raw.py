638b35dadfb908e8a91b98cc42ff442c9fd58c51
bernia <javibernia@gmail.com>
2021-07-15 16:18:31 +0200

    Added raw signal classifier

---
+++ python/train_classifier_raw.py
@@ -0,0 +1,113 @@
+import os
+import sys
+import numpy
+
+from pyeddl import eddl
+from pyeddl.tensor import Tensor
+
+from data_utils import DataGenerator, RawDataGenerator
+#from data_utils_eeg import PairDataGenerator
+from models_01 import model_classifier_1a, model_classifier_2a
+from sklearn.metrics import classification_report, confusion_matrix
+
+
+if __name__ == '__main__':
+
+    starting_epoch = 0
+    epochs = 10
+    batch_size = 100
+    model_filename = None
+    model_id = '1a'
+    index_filenames = list()
+    for i in range(1, len(sys.argv)):
+        param = sys.argv[i]
+        if param == '--model-filename':
+            model_filename = sys.argv[i+1]
+        elif param == '--model':
+            model_id = sys.argv[i+1]
+        elif param == '--starting-epoch':
+            starting_epoch = int(sys.argv[i+1])
+        elif param == '--epochs':
+            epochs = int(sys.argv[i+1])
+        elif param == '--batch-size':
+            batch_size = int(sys.argv[i+1])
+        elif sys.argv[i] == '--index':
+            index_filenames.append(sys.argv[i+1])
+    
+    dg = RawDataGenerator(index_filenames,
+                        batch_size = batch_size,
+                        window_length = 256 * 10,
+                        shift = 256 * 10,
+                        min_interictal_length = 256 * 3600 * 4, # 4 hours
+                        preictal_length = 256 * 3600, # 1 hour
+                        do_shuffle = True,
+                        mode = 'train')
+
+    print(f"Number of seizures: %d" % dg.num_seizures)
+
+    x, y = dg[0]
+    print(x.shape)
+    input_shape = (1,) + x.shape[1:]
+    if model_id == '1a':
+        net = model_classifier_1a(input_shape, num_classes = 2, filename = model_filename)
+    elif model_id == '2a':
+        net = model_classifier_2a(input_shape, num_classes = 2, filename = model_filename)
+    #elif model_id == '2':
+    #    net = model_2(input_shape, input_shape, filename = model_filename)
+    else:
+        raise Exception('You have to indicate a model id!')
+
+
+    log_file = open(f'log/model_classifier_{model_id}.log', 'a')
+
+
+    for fold in range(dg.num_seizures):
+        for epoch in range(starting_epoch, epochs):
+            print()
+            print('fold', fold, 'epoch:', epoch, 'num batches:', len(dg))
+            eddl.reset_loss(net)
+            
+            # Training
+            dg.mode = 'train'
+            for j in range(len(dg)):
+                x, y = dg[j]
+                x = numpy.expand_dims(x, axis = 1)
+                indices = list(range(len(x)))
+                x = Tensor.fromarray(x)
+                _y_ = numpy.zeros([len(y),2])
+                _y_[y == 0, 0] = 1
+                _y_[y == 1, 1] = 1
+                y = Tensor.fromarray(_y_)
+
+                eddl.train_batch(net, [x], [y], indices = indices)
+                eddl.print_loss(net, j)
+                print('\r', end = '')
+
+            log_file.write("fold %d    epoch %d   bce %g  acc %g\n" % (fold, epoch, eddl.get_losses(net)[0], eddl.get_metrics(net)[0]))
+            log_file.flush()
+            eddl.save_net_to_onnx_file(net, f'models/model_classifier_{model_id}-{epoch}.onnx')
+
+            # Validation
+            Y_true = list()
+            Y_pred = list()
+            dg.mode = 'test'
+            for j in range(len(dg)):
+                x, y_true = dg[j]
+                x = numpy.expand_dims(x, axis = 1)
+                indices = list(range(len(x)))
+                x = Tensor.fromarray(x)
+                (y_pred, ) = eddl.predict(net, [x])
+                y_pred = y_pred.getdata()
+                Y_true.append(y_true)
+                Y_pred.append(y_pred.argmax(axis = 1))
+
+            y_true = numpy.hstack(Y_true) * 1.0
+            y_pred = numpy.hstack(Y_pred) * 1.0
+            print('fold %d validation accuracy epoch %d = %g' % ( fold, epoch, sum(y_true == y_pred) / len(y_true)))
+            log_file.write('fold %d validation accuracy epoch %d = %g\n' % ( fold, epoch, sum(y_true == y_pred) / len(y_true))) # eddl.get_metrics(net)[0]))
+            print(confusion_matrix(y_true, y_pred, labels = [0, 1]))
+            print(classification_report(y_true, y_pred, target_names = ['inter-ictal', 'pre-ictal']))
+
+            dg.on_epoch_end()
+
+        log_file.close()
